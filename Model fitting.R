f343b5bc6387b38e2577a68e2a56ea3dd74903d5
Jan-Ole Koslik <janole.koslik@gmail.com>
2022-11-22 18:02:16 +0100

    Also fitting 4 state HMM

--- Model fitting.R
+++ Model fitting.R
@@ -21,7 +21,7 @@ colnames(d) = c("x", "y", "ID")
 d = prepData(d)
 
 
-# deleting wrong step lenghts and angles
+# deleting wrong step lengths and angles
 for (i in 1:N){
   d$step[30*i] = NA
 }
@@ -36,10 +36,13 @@ data2 = data %>%
   dplyr::select(step, angle, height = height.above.msl, height.fd, measurement, x, y) %>% 
   group_by(measurement) %>% 
   summarise(step = mean(step, na.rm = T)*1000, # converting to m/s
+            count_angles = sum(!is.na(angle)), # find intervals where there are barely any values
             angle = abs(mean(angle, na.rm = T))/pi,
             height.fd = mean(diff(height), na.rm = T),
             x = mean(x, na.rm = T),
             y = mean(y, na.rm = T))
+
+data2$angle[which(data2$count_angles <= 2)] = NA # set interval with barely any values to NA
 
 # Fitting a model with turning angle --------------------------------------
 
@@ -47,7 +50,7 @@ theta0 = c(rep(0.05, 6),
            9, 16, 0.3, # mu.gamma
            1.5, 6, 0.2, # sigma.gamma
            10, 1, 0.5, # alphas
-           55, 40, 1.5, # betas
+           55, 40, 2, # betas
            0.01, 0.05, 0.05, # zero masses
            1, -0.5, 0, # mu
            1, 1, 0.1) # sigma
@@ -56,24 +59,41 @@ theta.star0 = c(log(theta0[1:18]),
                 qlogis(theta0[19:21]),
                 theta0[22:24], 
                 log(theta0[25:27]))
-
+ 
 theta0.2 = c(rep(0.05, 6),
-           10, 16, 0.3, # mu.gamma
-           4, 6, 0.2, # sigma.gamma
+           9, 12, 0.3, # mu.gamma
+           1.5, 6, 0.2, # sigma.gamma
            10, 1, 0.5, # alphas
-           55, 40, 1.5, # betas
+           55, 40, 2, # betas
            0.01, 0.05, 0.05, # zero masses
-           1, -0.5, 0, # mu
-           1, 1, 0.1) # sigma
+           1, -1.8, 0, # mu
+           1, 0.6, 0.1) # sigma
 
 theta.star0.2 = c(log(theta0[1:18]),
                 qlogis(theta0[19:21]),
-                theta0[22:24], 
+                theta0[22:24],
                 log(theta0[25:27]))
+
+### 4 state HMM? 4-th state is flapping flight -> directed movement but upwards
+
+theta0.4 = c(rep(0.1, 12),
+             9, 12, 0.3, 9, # mu.gamma
+             1.5, 6, 0.2, 3, # sigma.gamma
+             10, 1, 0.5, 1, # alphas
+             55, 40, 2, 40, # betas
+             0.01, 0.01, 0.01, 0.01, # zero masses
+             1, -2, 0, 1, # mu
+             0.5, 0.5, 0.1, 0.5) # sigma
+
+theta.star0.4 = c(log(theta0.4[1:28]),
+                  qlogis(theta0.4[29:32]),
+                  theta0.4[33:36],
+                  log(theta0.4[37:40]))
 
 
 data2$height.fd[which(abs(data2$height.fd) > 20)] = NA
-data2$angle[which(data2$angle == 1)] = .99 # Wir brauchen zero und one inflated beta Verteilung??
+data2$angle[which(data2$angle == 1)] = runif(length(data2$angle[which(data2$angle == 1)]), 0.95, 1)
+# Wir brauchen zero und one inflated beta Verteilung??
 
 t1 = Sys.time()
 mod = nlm(f = mllk, p = theta.star0, X = data2, N = 3, print.level = 2, iterlim = 1000)
@@ -83,12 +103,19 @@ t1 = Sys.time()
 mod2 = nlm(f = mllk, p = theta.star0.2, X = data2, N = 3, print.level = 2, iterlim = 1000)
 Sys.time()-t1
 
+t1 = Sys.time()
+mod4 = nlm(f = mllk, p = theta.star0.4, X = data2, N = 4, print.level = 2, iterlim = 1000)
+Sys.time()-t1
+
 -mllk(mod$estimate, X = data2, N = 3)
--mllk(mod2$estimate, X = data2, N = 3) # same Maximum
+# -23738.48
+-mllk(mod4$estimate, X = data2, N = 4)
 
 theta.star = mod$estimate
+# theta.star = mod2$estimate
+theta.star = mod4$estimate
 
-N = 3
+N = 4
 
 Gamma = diag(N)
 Gamma[!Gamma] = exp(theta.star[1:((N-1)*N)])
@@ -134,20 +161,20 @@ sigma = exp(theta.star[(N-1)*N+6*N+1:N]) # sds of normal distributions
 
 # Plotting ----------------------------------------------------------------
 
-color = c("cornflowerblue", "orange", "forestgreen")
+color = c("deepskyblue", "orange", "forestgreen", "tomato3")
 
 par(mfrow = c(1,1))
 # total
 hist(data2$step, prob = T, breaks = 200, xlab = "Step length", xlim = c(0,30), main = "Resting")
 curve(delta[1]*dgamma(x, shape = mu.g[1]^2/sigma.g[1]^2, scale = sigma.g[1]^2/mu.g[1]), add = T, lwd = 2, col = color[1])
 curve(delta[2]*dgamma(x, shape = mu.g[2]^2/sigma.g[2]^2, scale = sigma.g[2]^2/mu.g[2]), add = T, lwd = 2, col = color[2])
-curve(delta[3]*dgamma(x, shape = mu.g[3]^2/sigma.g[3]^2, scale = sigma.g[3]^2/mu.g[3]), add = T, lwd = 2, col = color[3])
+curve(delta[3]*dgamma(x, shape = mu.g[3]^2/sigma.g[3]^2, scale = sigma.g[3]^2/mu.g[3]), add = T, lwd = 2, col = color[3],n = 500)
 
 curve(
   delta[1]*dgamma(x, shape = mu.g[1]^2/sigma.g[1]^2, scale = sigma.g[1]^2/mu.g[1])+
     delta[2]*dgamma(x, shape = mu.g[2]^2/sigma.g[2]^2, scale = sigma.g[2]^2/mu.g[2])+ 
     delta[3]*dgamma(x, shape = mu.g[3]^2/sigma.g[3]^2, scale = sigma.g[3]^2/mu.g[3]),
-  add = T, lty = "dashed", lwd = 2
+  add = T, lty = "dashed", lwd = 2, n = 500
 )
 
 hist(data2$angle, prob = T, breaks = 50, xlab = "Angle", xlim = c(0,1))
@@ -164,23 +191,84 @@ curve(
 # point masses on 0 and 1 for beta distribution!
 
 
-hist(data2$height.fd, prob = T, breaks = 100, xlab = "Height.fd")
+# Plotting 4 state HMM ----------------------------------------------------
+
+color = c("deepskyblue", "orange", "forestgreen", "tomato3")
+
+par(mfrow = c(1,1))
+# total
+hist(data2$step, prob = T, breaks = 200, xlab = "Step length", xlim = c(0,30), main = "Resting")
+curve(delta[1]*dgamma(x, shape = mu.g[1]^2/sigma.g[1]^2, scale = sigma.g[1]^2/mu.g[1]), add = T, lwd = 2, col = color[1])
+curve(delta[2]*dgamma(x, shape = mu.g[2]^2/sigma.g[2]^2, scale = sigma.g[2]^2/mu.g[2]), add = T, lwd = 2, col = color[2])
+curve(delta[3]*dgamma(x, shape = mu.g[3]^2/sigma.g[3]^2, scale = sigma.g[3]^2/mu.g[3]), add = T, lwd = 2, col = color[3],n = 500)
+curve(delta[4]*dgamma(x, shape = mu.g[4]^2/sigma.g[4]^2, scale = sigma.g[4]^2/mu.g[4]), add = T, lwd = 2, col = color[4],n = 500)
+
+curve(
+  delta[1]*dgamma(x, shape = mu.g[1]^2/sigma.g[1]^2, scale = sigma.g[1]^2/mu.g[1])+
+    delta[2]*dgamma(x, shape = mu.g[2]^2/sigma.g[2]^2, scale = sigma.g[2]^2/mu.g[2])+ 
+    delta[3]*dgamma(x, shape = mu.g[3]^2/sigma.g[3]^2, scale = sigma.g[3]^2/mu.g[3])+
+    delta[4]*dgamma(x, shape = mu.g[4]^2/sigma.g[4]^2, scale = sigma.g[4]^2/mu.g[4]),
+  add = T, lty = "dashed", lwd = 2, n = 500
+)
+
+hist(data2$angle, prob = T, breaks = 50, xlab = "Angle", xlim = c(0,1))
+curve(delta[1]*dbeta(x, shape1 = alpha[1], shape2 = beta[1]), add = T, lwd = 2, col = color[1])
+curve(delta[2]*dbeta(x, shape1 = alpha[2], shape2 = beta[2]), add = T, lwd = 2, col = color[2])
+curve(delta[3]*dbeta(x, shape1 = alpha[3], shape2 = beta[3]), add = T, lwd = 2, col = color[3])
+curve(delta[4]*dbeta(x, shape1 = alpha[4], shape2 = beta[4]), add = T, lwd = 2, col = color[4], n = 500)
+
+curve(
+  delta[1]*dbeta(x, shape1 = alpha[1], shape2 = beta[1])+
+    delta[2]*dbeta(x, shape1 = alpha[2], shape2 = beta[2])+
+    delta[3]*dbeta(x, shape1 = alpha[3], shape2 = beta[3])+
+    delta[4]*dbeta(x, shape1 = alpha[4], shape2 = beta[4]),
+  add = T, lty = "dashed", lwd = 2
+)
+
+
+
+hist(data2$height.fd, prob = T, breaks = 100, xlab = "Height.fd", xlim = c(-6,6))
 
 curve(delta[1]*dnorm(x, mu[1], sigma[1]), add = T, lwd = 2, col = color[1])
 curve(delta[2]*dnorm(x, mu[2], sigma[2]), add = T, lwd = 2, col = color[2])
 curve(delta[3]*dnorm(x, mu[3], sigma[3]), add = T, lwd = 2, col = color[3],n =1000)
+curve(delta[4]*dnorm(x, mu[4], sigma[4]), add = T, lwd = 2, col = color[4],n =1000)
 
 curve(
   delta[1]*dnorm(x, mu[1], sigma[1])+
     delta[2]*dnorm(x, mu[2], sigma[2])+
-    delta[3]*dnorm(x, mu[3], sigma[3]),
+    delta[3]*dnorm(x, mu[3], sigma[3])+
+    delta[4]*dnorm(x, mu[4], sigma[4]),
   add = T, lty = "dashed", lwd = 2, n=1000
 )
 
-states = viterbi(mod$estimate, data2, 3)
+states = viterbi(mod4$estimate, data2, 4)
 
 par(mfrow = c(3,1))
-plot(data2$step[4700:5000], type = "h", col = color[states[4700:5000]])
-plot(data2$angle[4700:5000], type = "h", col = color[states[4700:5000]])
-plot(data2$height.fd[4700:5000], type = "h", col = color[states[4700:5000]])
+plot(data2$step[3001:6000], type = "h", col = color[states[3001:6000]])
+plot(data2$angle[3001:6000], type = "h", col = color[states[3001:6000]])
+plot(data2$height.fd[3001:6000], type = "h", col = color[states[3001:6000]])
 
+
+
+# Looking at weird turning angles -----------------------------------------
+
+pi_half_ind = which(data2$angle > 0.49 & data2$angle < 0.51)
+pi_ind = which(data2$angle > 0.97 & data2$angle <= 1)
+
+# Intervalle mit <= 2 Datenpunkten = NA setzen
+for (value in pi_half_ind){
+  print(d$angle[(value-30)+1:30])
+  Sys.sleep(1)
+}
+
+for (value in pi_ind){
+  print(d$angle[(value-30)+1:30])
+  Sys.sleep(1)
+}
+
+
+# Getting better starting values for beta distribution --------------------
+
+hist(data2$angle, prob = T, breaks = 50, xlab = "Angle", xlim = c(0,1))
+curve(dbeta(x, shape1 = .5, shape2 = 2), add = T)
