1a6cdf6e0e27a12abf7e4fe79b4364c8c6360d63
Jan-Ole Koslik <janole.koslik@gmail.com>
2022-12-14 13:27:19 +0100

    new file for model with ToD and temperature

--- Model temp and ToD.R
+++ Model temp and ToD.R
@@ -15,8 +15,27 @@ theta.star0.tt = c(theta0.tt[1:48],
                     theta0.tt[73:79])
 
 t1 = Sys.time()
-mod9 = nlm(f = mllk_tt, p = theta.star0.tt, X = data4, N = 4, print.level = 2, iterlim = 1000, steptol = 1e-8)
+mod9 = nlm(f = mllk_tt, p = theta.star0.tt, X = data4, N = 4, print.level = 2, iterlim = 2000, steptol = 1e-15)
 Sys.time()-t1
 theta.star = mod9$estimate
-states = viterbi_tod(mod8$estimate, X = data2[1:5000,], N = 4)2*mllk_tod(mod8$estimate, X = data2[1:5000,], N = 4) + 2*length(mod8$estimate)
+states = viterbi_tt(mod9$estimate, X = data2[1:5000,], N = 4)
 
+# AIC
+2*mllk_tt(mod9$estimate, X = data2[1:5000,], N = 4) + 2*length(mod9$estimate)
+# BIC
+2*mllk_tt(mod9$estimate, X = data2[1:5000,], N = 4) + log(nrow(data2[1:5000,]))*length(mod9$estimate)
+# BIC better than model only with ToD
+
+# Results -----------------------------------------------------------------
+
+color = c("deepskyblue", "orange", "springgreen4", "dodgerblue3")
+
+# Scatterplot
+
+data4 = data2[1:5000,]
+par(mfrow = c(3,1))
+plot(data4$step[1:5000], pch = 20, col = color[states[1:5000]], ylab = "Step lenght")
+legend(x = 4700, y = 30, legend=c("State 1", "State 2", "State 3", "State 4"),
+       col=color, lty = 1, cex=1)
+plot(data4$angle[1:5000], pch = 20, col = color[states[1:5000]], ylab = "Turning angle")
+plot(data4$height.fd[1:5000], pch = 20, col = color[states[1:5000]], ylab = "Height (fd.)")
