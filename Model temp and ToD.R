5940761ca72a35501ef7c4f0a7ce7e02c42f598c
Jan-Ole Koslik <janole.koslik@gmail.com>
2022-12-15 14:55:29 +0100

    plotting

--- Model temp and ToD.R
+++ Model temp and ToD.R
@@ -39,3 +39,64 @@ legend(x = 4700, y = 30, legend=c("State 1", "State 2", "State 3", "State 4"),
        col=color, lty = 1, cex=1)
 plot(data4$angle[1:5000], pch = 20, col = color[states[1:5000]], ylab = "Turning angle")
 plot(data4$height.fd[1:5000], pch = 20, col = color[states[1:5000]], ylab = "Height (fd.)")
+
+
+# Hypothetical stationary
+
+todseq = seq(min(data4$time), max(data4$time), length.out = 500)
+tempseq = seq(min(data4$temp), max(data4$temp), length.out = 500)
+
+h_delta1 = solve_gamma_tt1(mod9$estimate, todseq, mean(data4$temp), 4)
+h_delta2 = solve_gamma_tt2(mod9$estimate, tempseq, mean(data4$time), 4)
+
+# Plot for time of day
+dev.off()
+par(mfrow = c(1,2))
+plot(todseq, h_delta1[,1], type = "l", lwd = 2, col = color[1], ylim = c(0,1), main = "Hypothetical stationary distribution", ylab = "Stationary state probabilities", xlab = "time of day")
+lines(todseq, h_delta1[,2], lwd = 2, col = color[2])
+lines(todseq, h_delta1[,3], lwd = 2, col = color[3])
+lines(todseq, h_delta1[,4], lwd = 2, col = color[4])
+legend(8, 1, legend=c("State 1", "State 2", "State 3", "State 4"),
+       col=color, lty = 1, cex=1, box.lwd = 0)
+
+# Plot for temperature
+plot(tempseq, h_delta2[,1], type = "l", lwd = 2, col = color[1], ylim = c(0,1), main = "Hypothetical stationary distribution", ylab = "Stationary state probabilities", xlab = "temperature")
+lines(tempseq, h_delta2[,2], lwd = 2, col = color[2])
+lines(tempseq, h_delta2[,3], lwd = 2, col = color[3])
+lines(tempseq, h_delta2[,4], lwd = 2, col = color[4])
+legend(15, 1, legend=c("State 1", "State 2", "State 3", "State 4"),
+       col=color, lty = 1, cex=1, box.lwd = 0)
+
+
+# Transition probabilities
+
+transprobs_tt1 = get_transprobs_tt1(mod9$estimate, todseq, mean(data4$temp))
+transprobs_tt2 = get_transprobs_tt2(mod9$estimate, tempseq, mean(data4$time))
+
+# Plot for time of day
+par(mfrow = c(4,4))
+par(mar = c(4.5, 4.5, 1.5, 2))
+for (i in 1:16){
+  if(i %in% 1:4){st = 1}
+  if(i %in% 5:8){st = 2}
+  if(i %in% 9:12){st = 3}
+  if(i %in% 13:16){st = 4}
+  plot(todseq, transprobs_tt1[,i], main = NULL, 
+       ylab = colnames(transprobs_tt1)[i], xlab = "time of day", 
+       type = "l", lwd = 2, col = color[st], ylim = c(0,1))
+}
+
+# Plot for temperature
+par(mfrow = c(4,4))
+par(mar = c(4.5, 4.5, 1.5, 2))
+for (i in 1:16){
+  if(i %in% 1:4){st = 1}
+  if(i %in% 5:8){st = 2}
+  if(i %in% 9:12){st = 3}
+  if(i %in% 13:16){st = 4}
+  plot(tempseq, transprobs_tt2[,i], main = NULL, 
+       ylab = colnames(transprobs_tt2)[i], xlab = "temperature", 
+       type = "l", lwd = 2, col = color[st], ylim = c(0,1))
+}
+
+
