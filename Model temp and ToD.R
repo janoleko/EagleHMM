35fb19a707a6ee4ad12f415266f7a8b935b6ecce
Jan-Ole Koslik <janole.koslik@gmail.com>
2023-01-21 14:53:46 +0100

    .

--- Model temp and ToD.R
+++ Model temp and ToD.R
@@ -1,8 +1,8 @@
 theta0.tt = c(rep(log(0.05), 12), rep(0,36),
                1, 8, 15, 2, # mu.gamma
                1, 4, 10, 2, # sigma.gamma
-               0.7, 10, 1, 3, # alphas
-               2, 55, 40, 40, # betas
+               0.3, 9, 1, 3, # alphas
+               2, 50, 40, 40, # betas
                0, 0.2, -0.2, 0.4, # xi
                0.05, 0.5, 0.5, 0.5, # omega
                0, 5, -5, 5, # al
@@ -24,14 +24,14 @@ mod9 = nlm(f = mllk_tt, p = theta.star0.tt, X = data4, N = 4, print.level = 2, i
 Sys.time()-t1
 
 
-mod9 = readRDS("mod9.rds")
+#mod9 = readRDS("mod9.rds")
 theta.star = mod9$estimate
-states = viterbi_tt(mod9$estimate, X = data2[1:5000,], N = 4)
+states = viterbi_tt(theta.star, X = data4, N = 4)
 
 # AIC
-2*mllk_tt(mod9$estimate, X = data2[1:5000,], N = 4) + 2*length(mod9$estimate)
+2*mod9$minimum + 2*length(mod9$estimate)
 # BIC
-2*mllk_tt(mod9$estimate, X = data2[1:5000,], N = 4) + log(nrow(data2[1:5000,]))*length(mod9$estimate)
+2*mod9$minimum + log(nrow(data4))*length(mod9$estimate)
 # BIC better than model only with ToD
 
 # Results -----------------------------------------------------------------
@@ -40,17 +40,15 @@ color = c("deepskyblue", "orange", "springgreen4", "dodgerblue3")
 
 # Scatterplot
 
-data4 = data2[1:5000,]
 par(mfrow = c(3,1))
-plot(data4$step[1:5000], pch = 20, col = color[states[1:5000]], ylab = "Step length")
-legend(x = 4700, y = 30, legend=c("State 1", "State 2", "State 3", "State 4"),
+plot(data4$step, pch = 20, col = color[states], ylab = "Step length")
+legend(x = 4200, y = 30, legend=c("State 1", "State 2", "State 3", "State 4"),
        col=color, lty = 1, cex=1)
-plot(data4$angle[1:5000], pch = 20, col = color[states[1:5000]], ylab = "Turning angle")
-plot(data4$height.fd[1:5000], pch = 20, col = color[states[1:5000]], ylab = "Height (fd.)")
+plot(data4$angle, pch = 20, col = color[states], ylab = "Turning angle")
+plot(data4$height.fd, pch = 20, col = color[states], ylab = "Height (fd.)")
 
 par(mfrow = c(1,1))
 
-plot(data4$height.fd[2000:2350], pch = 20, col = color[states[2000:2350]], ylab = "Height (fd.)")
 # weirder Zeitraum ist 2000-2350
 
 data4$day[2000:2350]
@@ -115,4 +113,54 @@ for (i in 1:16){
 }
 
 
+# Plot marginals ----------------------------------------------------------
+N = 4
+
+mu.g = exp(theta.star[4*(N-1)*N+1:N]) # means of gamma distributions
+sigma.g = exp(theta.star[4*(N-1)*N+N+1:N]) # sds of gamma distributions
+# beta distribution: Turning angle/ pi
+alpha = exp(theta.star[4*(N-1)*N+2*N+1:N]) # shape1 parameters of beta distributions
+beta = exp(theta.star[4*(N-1)*N+3*N+1:N]) # shape2 parameters of beta distributions
+# skew normal distribution: Height first difference
+xi = theta.star[4*(N-1)*N+4*N+1:N] # means of normal distributions
+omega = exp(theta.star[4*(N-1)*N+5*N+1:N]) # sds of normal distributions
+al = theta.star[4*(N-1)*N+6*N+1:N]
+
+delta_hat = c(sum(states==1), sum(states==2), sum(states==3), sum(states==4))/length(states)
+
+par(mfrow = c(1,1))
+hist(data4$step, prob = T, breaks = 50)
+curve(delta_hat[1]*dgamma(x, shape = mu.g[1]^2/sigma.g[1]^2, scale = sigma.g[1]^2/mu.g[1]), add = T, lwd = 2, col = color[1], n = 500)
+curve(delta_hat[2]*dgamma(x, shape = mu.g[2]^2/sigma.g[2]^2, scale = sigma.g[2]^2/mu.g[2]), add = T, lwd = 2, col = color[2], n = 500)
+curve(delta_hat[3]*dgamma(x, shape = mu.g[3]^2/sigma.g[3]^2, scale = sigma.g[3]^2/mu.g[3]), add = T, lwd = 2, col = color[3], n = 500)
+curve(delta_hat[4]*dgamma(x, shape = mu.g[4]^2/sigma.g[4]^2, scale = sigma.g[4]^2/mu.g[4]), add = T, lwd = 2, col = color[4], n = 500)
+curve(delta_hat[1]*dgamma(x, shape = mu.g[1]^2/sigma.g[1]^2, scale = sigma.g[1]^2/mu.g[1])+
+        delta_hat[2]*dgamma(x, shape = mu.g[2]^2/sigma.g[2]^2, scale = sigma.g[2]^2/mu.g[2])+
+        delta_hat[3]*dgamma(x, shape = mu.g[3]^2/sigma.g[3]^2, scale = sigma.g[3]^2/mu.g[3])+
+        delta_hat[4]*dgamma(x, shape = mu.g[4]^2/sigma.g[4]^2, scale = sigma.g[4]^2/mu.g[4]), add = T, lwd = 2, lty = "dashed")
+
+hist(data4$angle, prob = T, breaks = 100)
+curve(delta_hat[1]*dbeta(x, shape1 = alpha[1], shape2 = beta[1]), add = T, col = color[1], lwd = 2)
+curve(delta_hat[2]*dbeta(x, shape1 = alpha[2], shape2 = beta[2]), add = T, col = color[2], lwd = 2)
+curve(delta_hat[3]*dbeta(x, shape1 = alpha[3], shape2 = beta[3]), add = T, col = color[3], lwd = 2)
+curve(delta_hat[4]*dbeta(x, shape1 = alpha[4], shape2 = beta[4]), add = T, col = color[4], lwd = 2)
+curve(delta_hat[1]*dbeta(x, shape1 = alpha[1], shape2 = beta[1])+
+        delta_hat[2]*dbeta(x, shape1 = alpha[2], shape2 = beta[2])+
+        delta_hat[3]*dbeta(x, shape1 = alpha[3], shape2 = beta[3])+
+        delta_hat[4]*dbeta(x, shape1 = alpha[4], shape2 = beta[4]), add = T, lwd = 2, lty = "dashed")
+
+hist(data4$height.fd, prob = T, breaks = 100, xlim = c(-5,5))
+curve(delta_hat[1]*dsn(x, xi[1], omega[1], al[1]), add = T, lwd = 2, col = color[1])
+curve(delta_hat[2]*dsn(x, xi[2], omega[2], al[2]), add = T, lwd = 2, col = color[2])
+curve(delta_hat[3]*dsn(x, xi[3], omega[3], al[3]), add = T, lwd = 2, col = color[3])
+curve(delta_hat[4]*dsn(x, xi[4], omega[4], al[4]), add = T, lwd = 2, col = color[4])
+curve(delta_hat[1]*dsn(x, xi[1], omega[1], al[1])+
+        delta_hat[2]*dsn(x, xi[2], omega[2], al[2])+
+        delta_hat[3]*dsn(x, xi[3], omega[3], al[3])+
+        delta_hat[4]*dsn(x, xi[4], omega[4], al[4]), add = T, lwd = 2, lty = "dashed")
+
+
+
+hist(data4$angle, prob = T, breaks = 100)
+curve(delta_hat[1]*dbeta(x, shape1 = 0.3, shape2 = 2), add = T, col = color[2], lwd = 2)
 
