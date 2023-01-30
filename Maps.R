1b6755bafd0e0998e7b6989aa56587f5e55d9dc6
Jan-Ole Koslik <janole.koslik@gmail.com>
2023-01-30 09:38:47 +0100

    Adding maps with decoded states

--- Maps.R
+++ Maps.R
@@ -2,5 +2,7 @@
 # Plot decoded states on map ----------------------------------------------
 
 library(ggmap)
-ggmap(get_map(location = c(75, 20, 110, 50), map_type = "terrain", source = "stamen"))
+ggmap(get_map(location = c(75, 20, 110, 50), map_type = "terrain", source = "stamen"))+
+  geom_point(data = data4, mapping = aes(x = x, y = y, colour = color[states]))
+
 
