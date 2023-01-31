7e7b4aadd1d3d1ef670804ab985dff8bbef878ef
Jan-Ole Koslik <janole.koslik@gmail.com>
2023-01-31 18:52:54 +0100

    maps with resting locations

--- Maps.R
+++ Maps.R
@@ -3,6 +3,14 @@
 
 library(ggmap)
 ggmap(get_map(location = c(75, 20, 110, 50), map_type = "terrain", source = "stamen"))+
-  geom_point(data = data4, mapping = aes(x = x, y = y, colour = color[states]))
+  geom_point(data = data7, mapping = aes(x = x, y = y, colour = color[states]))
 
+data8 = data7[which(states == 1),]
 
+ggmap(get_map(location = c(75, 20, 110, 50), map_type = "terrain", source = "stamen"))+
+  geom_point(data = data8, mapping = aes(x = x, y = y, colour = "skyblue"))
+
+data9 = data7[which(states == 4),]
+
+ggmap(get_map(location = c(75, 20, 110, 50), map_type = "terrain", source = "stamen"))+
+  geom_point(data = data9, mapping = aes(x = x, y = y))
