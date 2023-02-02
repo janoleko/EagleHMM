7cd421a9fce8488514f1d2b77a4f458db9877e2a
Jan-Ole Koslik <janole.koslik@gmail.com>
2023-02-02 09:13:34 +0100

    Created more beautiful maps

--- Maps.R
+++ Maps.R
@@ -1,16 +1,23 @@
 
 # Plot decoded states on map ----------------------------------------------
-
 library(ggmap)
-ggmap(get_map(location = c(75, 20, 110, 50), map_type = "terrain", source = "stamen"))+
-  geom_point(data = data7, mapping = aes(x = x, y = y, colour = color[states]))
 
+# overall
+ggmap(get_map(location = c(75, 20, 110, 50), map_type = "toner", source = "stamen"))+
+  geom_path(data = data7, aes(x = x, y = y), linewidth = 4, lineend = "round")+
+  geom_path(data = data7, aes(x = x, y = y), linewidth = 1.5, alpha = 0.8, lineend = "round", linejoin = "mitre", colour = color[states])
+
+# resting
 data8 = data7[which(states == 1),]
-
 ggmap(get_map(location = c(75, 20, 110, 50), map_type = "terrain", source = "stamen"))+
-  geom_point(data = data8, mapping = aes(x = x, y = y, colour = "skyblue"))
+  geom_point(data = data8, mapping = aes(x = x, y = y), colour = "deepskyblue4", size = 2.5) +
+  geom_point(data = data8, mapping = aes(x = x, y = y), colour = "deepskyblue", size = 1.5) +
+  labs(title = "Resting locations")
 
+# state 4
 data9 = data7[which(states == 4),]
-
 ggmap(get_map(location = c(75, 20, 110, 50), map_type = "terrain", source = "stamen"))+
-  geom_point(data = data9, mapping = aes(x = x, y = y))
+  geom_point(data = data9, mapping = aes(x = x, y = y), colour = "dodgerblue4", size = 2.5) +
+  geom_point(data = data9, mapping = aes(x = x, y = y), colour = "dodgerblue", size = 1.5) +
+  labs(title = "State 4 locations")
+
