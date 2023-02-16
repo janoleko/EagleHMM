3177030db9ecd937ebdc4738767813fb98154f6a
Jan-Ole Koslik <janole.koslik@gmail.com>
2023-02-16 13:34:01 +0100

    Updating maps

--- Maps.R
+++ Maps.R
@@ -4,20 +4,23 @@ library(ggmap)
 
 # overall
 ggmap(get_map(location = c(75, 20, 110, 50), map_type = "toner", source = "stamen"))+
+  annotate("rect", xmin = 75, xmax = 110, ymin = 20, ymax = 50, fill = "white", alpha = 0.4)+
   geom_path(data = data7, aes(x = x, y = y), linewidth = 4, lineend = "round")+
-  geom_path(data = data7, aes(x = x, y = y), linewidth = 1.5, alpha = 0.8, lineend = "round", linejoin = "mitre", colour = color[states])
+  geom_path(data = data7, aes(x = x, y = y), linewidth = 1.5, alpha = 0.9, lineend = "round", linejoin = "mitre", colour = color[states])
 
 # resting
 data8 = data7[which(states == 1),]
 ggmap(get_map(location = c(75, 20, 110, 50), map_type = "terrain", source = "stamen"))+
-  geom_point(data = data8, mapping = aes(x = x, y = y), colour = "deepskyblue4", size = 2.5) +
-  geom_point(data = data8, mapping = aes(x = x, y = y), colour = "deepskyblue", size = 1.5) +
+  annotate("rect", xmin = 75, xmax = 110, ymin = 20, ymax = 50, fill = "white", alpha = 0.4)+
+  geom_point(data = data8, mapping = aes(x = x, y = y), colour = "deepskyblue4", size = 3.5) +
+  geom_point(data = data8, mapping = aes(x = x, y = y), colour = "deepskyblue", size = 2) +
   labs(title = "Resting locations")
 
 # state 4
 data9 = data7[which(states == 4),]
 ggmap(get_map(location = c(75, 20, 110, 50), map_type = "terrain", source = "stamen"))+
-  geom_point(data = data9, mapping = aes(x = x, y = y), colour = "dodgerblue4", size = 2.5) +
-  geom_point(data = data9, mapping = aes(x = x, y = y), colour = "dodgerblue", size = 1.5) +
-  labs(title = "State 4 locations")
+  annotate("rect", xmin = 75, xmax = 110, ymin = 20, ymax = 50, fill = "white", alpha = 0.4)+
+  geom_point(data = data9, mapping = aes(x = x, y = y), colour = "dodgerblue4", size = 3.5) +
+  geom_point(data = data9, mapping = aes(x = x, y = y), colour = "dodgerblue", size = 2) +
+  labs(title = "Locations of state 4")
 
