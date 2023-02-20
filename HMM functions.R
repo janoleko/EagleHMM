39dbd436ba1bb8511a28970e5fd4bf9a79f837ae
Jan-Ole Koslik <janole.koslik@gmail.com>
2023-02-20 17:12:47 +0100

    .

--- HMM functions.R
+++ HMM functions.R
@@ -554,57 +554,47 @@ mllk_tod = function(theta.star, X, N){
 mllk_tt = function(theta.star, X, N){
   days = unique(X$day)
   numdays = length(days)
-  
   coef = matrix(theta.star[1:(4*(N-1)*N)], (N-1)*N, 4)
   
-  # gamma distribution: Step length
   mu.g = exp(theta.star[4*(N-1)*N+1:N]) # means of gamma distributions
   sigma.g = exp(theta.star[4*(N-1)*N+N+1:N]) # sds of gamma distributions
   
-  # beta distribution: Turning angle/ pi
   alpha = exp(theta.star[4*(N-1)*N+2*N+1:N]) # shape1 parameters of beta distributions
   beta = exp(theta.star[4*(N-1)*N+3*N+1:N]) # shape2 parameters of beta distributions
   
-  # skew normal distribution: Height first difference
-  xi = theta.star[4*(N-1)*N+4*N+1:N] # means of normal distributions
-  omega = exp(theta.star[4*(N-1)*N+5*N+1:N]) # sds of normal distributions
+  xi = theta.star[4*(N-1)*N+4*N+1:N]
+  omega = exp(theta.star[4*(N-1)*N+5*N+1:N])
   al = theta.star[4*(N-1)*N+6*N+1:N]
   
   delta = c(1, exp(theta.star[4*(N-1)*N+7*N+1:(N-1)]))
   delta = delta/sum(delta)
   
   allprobs = matrix(1, nrow(X), N)
-  ind1 = which(!is.na(X$step) & !is.na(X$angle) & !is.na(X$height.fd)) # Fragen wie wir das machen --> sonst immer gar keine Information nur weil in angle Zeitreihe so viele NAs
+  ind1 = which(!is.na(X$step) & !is.na(X$angle) & !is.na(X$height.fd))
   ind2 = which(!is.na(X$step) & is.na(X$angle) & !is.na(X$height.fd))
   
   for (j in 1:N){ # allprobs matrix
     allprobs[ind1,j] =
-      dgamma(X$step[ind1], shape = mu.g[j]^2/sigma.g[j]^2, scale = sigma.g[j]^2/mu.g[j])* #gamma
-      dbeta(X$angle[ind1], shape1 = alpha[j], shape2 = beta[j])* # beta
-      dsn(X$height.fd[ind1], xi = xi[j], omega = omega[j], alpha = al[j]) # skew normal
-    
+      dgamma(X$step[ind1], shape = mu.g[j]^2/sigma.g[j]^2, scale = sigma.g[j]^2/mu.g[j])*
+      dbeta(X$angle[ind1], shape1 = alpha[j], shape2 = beta[j])*
+      sn::dsn(X$height.fd[ind1], xi = xi[j], omega = omega[j], alpha = al[j])
     allprobs[ind2,j] =
-      dgamma(X$step[ind2], shape = mu.g[j]^2/sigma.g[j]^2, scale = sigma.g[j]^2/mu.g[j])* #gamma
-      dsn(X$height.fd[ind2], xi = xi[j], omega = omega[j], alpha = al[j]) # skew normal
+      dgamma(X$step[ind2], shape = mu.g[j]^2/sigma.g[j]^2, scale = sigma.g[j]^2/mu.g[j])*
+      sn::dsn(X$height.fd[ind2], xi = xi[j], omega = omega[j], alpha = al[j])
   }
   
   # forward algorithm to compute the log-likelihood
   l = numeric(numdays)
-  # compute the daywise likelihoods seperately and sum up in the end
-  for(i in 1:numdays){
+  for(i in 1:numdays){ # days independent
     index = which(X$day == days[i])
     
-    eta = coef[,1] + coef[,2]*sin(2*pi*X$time[index[1]]/24) + coef[,3]*cos(2*pi*X$time[index[1]]/24) + 
-      coef[,4]*X$temp[index[1]]
-    
-    # vllt statt delta die hypothetische stationary zu Tagesbeginn oder gleichverteilt
     foo = delta%*%diag(allprobs[index[1],])
     l[i] = log(sum(foo))
     phi = foo/sum(foo)
     
     for (t in 2:length(index)){
-      eta = coef[,1] + coef[,2]*sin(2*pi*X$time[index[t]]/24) + coef[,3]*cos(2*pi*X$time[index[t]]/24) +
-        coef[,4]*X$temp[index[t]]
+      eta = coef[,1] + coef[,2]*sin(2*pi*X$time[index[t]]/24) + coef[,3]*cos(2*pi*X$time[index[t]]/24) + coef[,4]*X$temp[index[t]]
+      
       Gamma = diag(N)
       Gamma[!Gamma] = exp(eta) # dynamically changing Gamma-Matrix
       Gamma = Gamma/rowSums(Gamma)
@@ -895,52 +885,40 @@ viterbi_tod = function(theta.star, X, N){
   n = nrow(X)
   days = unique(X$day)
   numdays = length(days)
-  
   coef = matrix(theta.star[1:(3*(N-1)*N)], (N-1)*N, 3)
   
-  # gamma distribution: Step length
   mu.g = exp(theta.star[3*(N-1)*N+1:N]) # means of gamma distributions
   sigma.g = exp(theta.star[3*(N-1)*N+N+1:N]) # sds of gamma distributions
   
-  # beta distribution: Turning angle/ pi
   alpha = exp(theta.star[3*(N-1)*N+2*N+1:N]) # shape1 parameters of beta distributions
   beta = exp(theta.star[3*(N-1)*N+3*N+1:N]) # shape2 parameters of beta distributions
   
-  # normal distribution: Height first difference
-  xi = theta.star[3*(N-1)*N+4*N+1:N] # means of normal distributions
-  omega = exp(theta.star[3*(N-1)*N+5*N+1:N]) # sds of normal distributions
+  xi = theta.star[3*(N-1)*N+4*N+1:N]
+  omega = exp(theta.star[3*(N-1)*N+5*N+1:N])
   al = theta.star[3*(N-1)*N+6*N+1:N]
   
+  delta = c(1, exp(theta.star[3*(N-1)*N+7*N+1:(N-1)]))
+  delta = delta/sum(delta)
+  
   allprobs = matrix(1, nrow(X), N)
-  ind1 = which(!is.na(X$step) & !is.na(X$angle) & !is.na(X$height.fd)) # Fragen wie wir das machen --> sonst immer gar keine Information nur weil in angle Zeitreihe so viele NAs
+  ind1 = which(!is.na(X$step) & !is.na(X$angle) & !is.na(X$height.fd))
   ind2 = which(!is.na(X$step) & is.na(X$angle) & !is.na(X$height.fd))
   
   for (j in 1:N){ # allprobs matrix
     allprobs[ind1,j] =
-      dgamma(X$step[ind1], shape = mu.g[j]^2/sigma.g[j]^2, scale = sigma.g[j]^2/mu.g[j])* #gamma
-      dbeta(X$angle[ind1], shape1 = alpha[j], shape2 = beta[j])* # beta
-      dsn(X$height.fd[ind1], xi = xi[j], omega = omega[j], alpha = al[j]) # skew normal
-    
+      dgamma(X$step[ind1], shape = mu.g[j]^2/sigma.g[j]^2, scale = sigma.g[j]^2/mu.g[j])*
+      dbeta(X$angle[ind1], shape1 = alpha[j], shape2 = beta[j])*
+      sn::dsn(X$height.fd[ind1], xi = xi[j], omega = omega[j], alpha = al[j])
     allprobs[ind2,j] =
-      dgamma(X$step[ind2], shape = mu.g[j]^2/sigma.g[j]^2, scale = sigma.g[j]^2/mu.g[j])* #gamma
-      dsn(X$height.fd[ind2], xi = xi[j], omega = omega[j], alpha = al[j]) # skew normal
+      dgamma(X$step[ind2], shape = mu.g[j]^2/sigma.g[j]^2, scale = sigma.g[j]^2/mu.g[j])*
+      sn::dsn(X$height.fd[ind2], xi = xi[j], omega = omega[j], alpha = al[j])
   }
   
-  # forward algorithm to compute the log-likelihood
-  l = numeric(numdays)
   xi = matrix(0, n, ncol = N)
 
   for(i in 1:numdays){
-    index = which(X$day == i)
-    
-    eta = coef[,1] + coef[,2]*sin(2*pi*X$time[index[1]]/24) + coef[,3]*cos(2*pi*X$time[index[1]]/24)
-    
-    Gamma = diag(N)
-    Gamma[!Gamma] = exp(eta) # dynamically changing Gamma-Matrix
-    Gamma = Gamma/rowSums(Gamma)
-    delta = solve(t(diag(N)-Gamma+1),rep(1,N), tol = 1e-18)
-    
-    # vllt statt delta die hypothetische stationary zu Tagesbeginn oder gleichverteilt
+    index = which(X$day == days[i])
+
     foo = delta * allprobs[index[1], ]
     xi[index[1], ] = foo / sum(foo)
     
@@ -971,20 +949,16 @@ viterbi_tt = function(theta.star, X, N){
   n = nrow(X)
   days = unique(X$day)
   numdays = length(days)
-  
   coef = matrix(theta.star[1:(4*(N-1)*N)], (N-1)*N, 4)
   
-  # gamma distribution: Step length
   mu.g = exp(theta.star[4*(N-1)*N+1:N]) # means of gamma distributions
   sigma.g = exp(theta.star[4*(N-1)*N+N+1:N]) # sds of gamma distributions
   
-  # beta distribution: Turning angle/ pi
   alpha = exp(theta.star[4*(N-1)*N+2*N+1:N]) # shape1 parameters of beta distributions
   beta = exp(theta.star[4*(N-1)*N+3*N+1:N]) # shape2 parameters of beta distributions
   
-  # normal distribution: Height first difference
-  xi = theta.star[4*(N-1)*N+4*N+1:N] # means of normal distributions
-  omega = exp(theta.star[4*(N-1)*N+5*N+1:N]) # sds of normal distributions
+  xi = theta.star[4*(N-1)*N+4*N+1:N]
+  omega = exp(theta.star[4*(N-1)*N+5*N+1:N])
   al = theta.star[4*(N-1)*N+6*N+1:N]
   
   delta = c(1, exp(theta.star[4*(N-1)*N+7*N+1:(N-1)]))
@@ -1005,19 +979,10 @@ viterbi_tt = function(theta.star, X, N){
       dsn(X$height.fd[ind2], xi = xi[j], omega = omega[j], alpha = al[j]) # skew normal
   }
   
-  # forward algorithm to compute the log-likelihood
-  l = numeric(numdays)
   xi = matrix(0, n, ncol = N)
   
   for(i in 1:numdays){
     index = which(X$day == days[i])
-    
-    eta = coef[,1] + coef[,2]*sin(2*pi*X$time[index[1]]/24) + coef[,3]*cos(2*pi*X$time[index[1]]/24) + 
-      coef[,4]*X$temp[index[1]]
-    
-    Gamma = diag(N)
-    Gamma[!Gamma] = exp(eta) # dynamically changing Gamma-Matrix
-    Gamma = Gamma/rowSums(Gamma)
 
     foo = delta * allprobs[index[1], ]
     xi[index[1], ] = foo / sum(foo)
@@ -1026,7 +991,7 @@ viterbi_tt = function(theta.star, X, N){
       eta = coef[,1] + coef[,2]*sin(2*pi*X$time[index[t]]/24) + coef[,3]*cos(2*pi*X$time[index[t]]/24) + 
         coef[,4]*X$temp[index[t]]
       Gamma = diag(N)
-      Gamma[!Gamma] = exp(eta) # dynamically changing Gamma-Matrix
+      Gamma[!Gamma] = exp(eta)
       Gamma = Gamma/rowSums(Gamma)
       
       foo = apply(xi[index[t - 1], ] * Gamma, 2, max) * allprobs[index[t], ]
--- Models for both ways.R
+++ Models for both ways.R
@@ -39,10 +39,197 @@ theta.star0.tod = c(theta0.tod[1:36],
 
 # Parallelization
 
+# cl = makeCluster(8); setDefaultCluster(cl=cl)
+# t1 = Sys.time()
+# mod_tod_bothways = optimParallel(par = theta.star0.tod, fn = mllk_tod, X = data7, N = 4, hessian = T, control = list(trace = 5, maxit = 10000))
+# Sys.time()-t1
+# stopCluster(cl)
+
+# saveRDS(mod_tod_bothways, "mod_tod_bothways.rds")
+
+theta.star = mod_tod_bothways$par
+N = 4
+
+(AIC = 2*mod_tod_bothways$value + 2*length(theta.star))
+(BIC = 2*mod_tod_bothways$value + log(nrow(data7))*length(theta.star))
+
+coef = matrix(theta.star[1:(3*(N-1)*N)], (N-1)*N, 3)
+mu.g = exp(theta.star[3*(N-1)*N+1:N]) # means of gamma distributions
+sigma.g = exp(theta.star[3*(N-1)*N+N+1:N]) # sds of gamma distributions
+alpha = exp(theta.star[3*(N-1)*N+2*N+1:N]) # shape1 parameters of beta distributions
+beta = exp(theta.star[3*(N-1)*N+3*N+1:N]) # shape2 parameters of beta distributions
+xi = theta.star[3*(N-1)*N+4*N+1:N]
+omega = exp(theta.star[3*(N-1)*N+5*N+1:N])
+al = theta.star[3*(N-1)*N+6*N+1:N]
+delta = c(1, exp(theta.star[3*(N-1)*N+7*N+1:(N-1)]))
+delta = delta/sum(delta)
+
+color = c("deepskyblue", "orange", "springgreen3", "dodgerblue3")
+
+states = viterbi_tod(theta.star, data7, N = 4)
+delta_star = c(sum(states == 1), sum(states == 2), sum(states == 3), sum(states == 4))/length(states)
+
+# Plotting results
+color = c("deepskyblue", "orange", "springgreen3", "dodgerblue3")
+
+par(mfrow = c(1,3))
+hist(data7$step, prob = T, breaks = 50, border = "white", main = NULL, col = "grey70", xlab = "step length", ylim = c(0,0.7))
+curve(delta_star[1]*dgamma(x, shape = mu.g[1]^2/sigma.g[1]^2, scale = sigma.g[1]^2/mu.g[1]), add = T, lwd = 2, col = color[1], n = 500)
+curve(delta_star[2]*dgamma(x, shape = mu.g[2]^2/sigma.g[2]^2, scale = sigma.g[2]^2/mu.g[2]), add = T, lwd = 2, col = color[2], n = 500)
+curve(delta_star[3]*dgamma(x, shape = mu.g[3]^2/sigma.g[3]^2, scale = sigma.g[3]^2/mu.g[3]), add = T, lwd = 2, col = color[3], n = 500)
+curve(delta_star[4]*dgamma(x, shape = mu.g[4]^2/sigma.g[4]^2, scale = sigma.g[4]^2/mu.g[4]), add = T, lwd = 2, col = color[4], n = 500)
+curve(delta_star[1]*dgamma(x, shape = mu.g[1]^2/sigma.g[1]^2, scale = sigma.g[1]^2/mu.g[1])+
+        delta_star[2]*dgamma(x, shape = mu.g[2]^2/sigma.g[2]^2, scale = sigma.g[2]^2/mu.g[2])+
+        delta_star[3]*dgamma(x, shape = mu.g[3]^2/sigma.g[3]^2, scale = sigma.g[3]^2/mu.g[3])+
+        delta_star[4]*dgamma(x, shape = mu.g[4]^2/sigma.g[4]^2, scale = sigma.g[4]^2/mu.g[4]), add = T, lwd = 2, lty = "dashed", n = 500)
+legend("topright", paste("state", c(1,2,3,4)), col = color, lwd = 2, bty = "n")
+
+
+hist(data7$angle, prob = T, breaks = 50, border = "white", main = NULL, col = "grey70", xlab = "turning angle", ylim = c(0,12))
+curve(delta_star[1]*dbeta(x, alpha[1], beta[1]), add = T, lwd = 2, col = color[1], n = 500)
+curve(delta_star[2]*dbeta(x, alpha[2], beta[2]), add = T, lwd = 2, col = color[2], n = 500)
+curve(delta_star[3]*dbeta(x, alpha[3], beta[3]), add = T, lwd = 2, col = color[3], n = 500)
+curve(delta_star[4]*dbeta(x, alpha[4], beta[4]), add = T, lwd = 2, col = color[4], n = 500)
+curve(delta_star[1]*dbeta(x, alpha[1], beta[1])+
+        delta_star[2]*dbeta(x, alpha[2], beta[2])+
+        delta_star[3]*dbeta(x, alpha[3], beta[3])+
+        delta_star[4]*dbeta(x, alpha[4], beta[4]), add = T, lwd = 2, lty = "dashed", n = 500)
+
+hist(data7$height.fd, prob = T, breaks = 200, xlim = c(-5,5), border = "white", main = NULL, col = "grey70", xlab = "height difference", ylim = c(0,1))
+curve(delta_star[1]*dsn(x, xi[1], omega[1], al[1]), add = T, lwd = 2, col = color[1], n = 500)
+curve(delta_star[2]*dsn(x, xi[2], omega[2], al[2]), add = T, lwd = 2, col = color[2], n = 500)
+curve(delta_star[3]*dsn(x, xi[3], omega[3], al[3]), add = T, lwd = 2, col = color[3], n = 500)
+curve(delta_star[4]*dsn(x, xi[4], omega[4], al[4]), add = T, lwd = 2, col = color[4], n = 500)
+curve(delta_star[1]*dsn(x, xi[1], omega[1], al[1])+
+        delta_star[2]*dsn(x, xi[2], omega[2], al[2])+
+        delta_star[3]*dsn(x, xi[3], omega[3], al[3])+
+        delta_star[4]*dsn(x, xi[4], omega[4], al[4]), add = T, lwd = 2, lty = "dashed", n = 500)
+
+
+par(mfrow = c(3,1))
+plot(data7$step, pch = 20, bty = "n", col = color[states], ylab = "step length")
+plot(data7$angle, pch = 20, bty = "n", col = color[states], ylab = "turning angle")
+plot(data7$height.fd, pch = 20, bty = "n", col = color[states], ylab = "height difference")
+
+
+## Dwell times
+
+par(mfrow = c(2,2))
+for (st in 1:4){
+  dwt = get_dwell_times(states, st)
+  dwf = get_dwell_freq(dwt)
+  
+  plot(ddwell(1:(24*4), state = st, N = 4, coef = coef), pch = 16, col = color[st], ylim = c(0,1), xlim = c(1,40),
+       ylab = paste0("d",st,"(r)"), xlab = "r", main = paste("Dwell times in state",st), bty = "n")
+  points(1:max(dwt), dwf, type = "h")
+  legend("topright", c("implied dwell times", "decoded"), bty = "n", 
+         pch = c(16,NA), lty = c(NA,1), col = c(color[st], "black"))
+}
+
+
+## Tranition probs
+todseq = seq(min(data7$time), max(data7$time), length.out = 500)
+transprobs_tod = get_transprobs_tod(theta.star, todseq)
+
+par(mfrow = c(4,4))
+par(mar = c(4.5, 4.5, 1.5, 2))
+for (i in 1:16){
+  if(i %in% 1:4){st = 1}
+  if(i %in% 5:8){st = 2}
+  if(i %in% 9:12){st = 3}
+  if(i %in% 13:16){st = 4}
+  plot(todseq, transprobs_tod[,i], main = NULL, 
+       ylab = colnames(transprobs_tod)[i], xlab = "time of day", 
+       type = "l", lwd = 2, col = color[st], ylim = c(0,1))
+}
+
+
+
+
+# Model with ToD and temp -------------------------------------------------
+
+theta0.tt = c(Gamma[-1,1], Gamma[-2,2], Gamma[-3,3], Gamma[-4,4], rep(0,36),
+               1, 8, 15, 2, # mu.gamma
+               1, 4, 10, 2, # sigma.gamma
+               0.7, 10, 1, 3, # alphas
+               2, 55, 40, 40, # betas
+               0, 0.2, -0.2, 0.4, # xi
+               0.05, 0.5, 0.5, 0.5, # omega
+               0, 5, -5, 5, # al
+               -2, -2, -2) # delta
+
+theta.star0.tt = c(theta0.tt[1:48],
+                    log(theta0.tt[49:64]),
+                    theta0.tt[65:68],
+                    log(theta0.tt[69:72]),
+                    theta0.tt[73:79])
+
 cl = makeCluster(8); setDefaultCluster(cl=cl)
 t1 = Sys.time()
-mod_tod_bothways = optimParallel(par = theta.star0.tod, fn = mllk_tod, X = data7, N = 4, hessian = T, control = list(trace = 5, maxit = 10000))
+mod_tt_bothways = optimParallel(par = theta.star0.tt, fn = mllk_tt, X = data7, N = 4, hessian = T, control = list(trace = 5, maxit = 10000))
 Sys.time()-t1
 stopCluster(cl)
 
-theta.star = mod_tod_bothways$par
+saveRDS(mod_tt_bothways, "mod_tt_bothways.rds")
+
+theta.star = mod_tt_bothways$par
+
+(AIC = 2*mod_tt_bothways$value + 2*length(theta.star))
+(BIC = 2*mod_tt_bothways$value + log(nrow(data7))*length(theta.star))
+
+N = 4
+coef = matrix(theta.star[1:(4*(N-1)*N)], (N-1)*N, 4)
+mu.g = exp(theta.star[4*(N-1)*N+1:N]) # means of gamma distributions
+sigma.g = exp(theta.star[4*(N-1)*N+N+1:N]) # sds of gamma distributions
+alpha = exp(theta.star[4*(N-1)*N+2*N+1:N]) # shape1 parameters of beta distributions
+beta = exp(theta.star[4*(N-1)*N+3*N+1:N]) # shape2 parameters of beta distributions
+xi = theta.star[4*(N-1)*N+4*N+1:N]
+omega = exp(theta.star[4*(N-1)*N+5*N+1:N])
+al = theta.star[4*(N-1)*N+6*N+1:N]
+delta = c(1, exp(theta.star[4*(N-1)*N+7*N+1:(N-1)]))
+delta = delta/sum(delta)
+
+states = viterbi_tt(theta.star, data7, N = 4)
+delta_star = c(sum(states == 1), sum(states == 2), sum(states == 3), sum(states == 4))/length(states)
+
+
+par(mfrow = c(1,3))
+hist(data7$step, prob = T, breaks = 50, border = "white", main = NULL, col = "grey70", xlab = "step length", ylim = c(0,0.7))
+curve(delta_star[1]*dgamma(x, shape = mu.g[1]^2/sigma.g[1]^2, scale = sigma.g[1]^2/mu.g[1]), add = T, lwd = 2, col = color[1], n = 500)
+curve(delta_star[2]*dgamma(x, shape = mu.g[2]^2/sigma.g[2]^2, scale = sigma.g[2]^2/mu.g[2]), add = T, lwd = 2, col = color[2], n = 500)
+curve(delta_star[3]*dgamma(x, shape = mu.g[3]^2/sigma.g[3]^2, scale = sigma.g[3]^2/mu.g[3]), add = T, lwd = 2, col = color[3], n = 500)
+curve(delta_star[4]*dgamma(x, shape = mu.g[4]^2/sigma.g[4]^2, scale = sigma.g[4]^2/mu.g[4]), add = T, lwd = 2, col = color[4], n = 500)
+curve(delta_star[1]*dgamma(x, shape = mu.g[1]^2/sigma.g[1]^2, scale = sigma.g[1]^2/mu.g[1])+
+        delta_star[2]*dgamma(x, shape = mu.g[2]^2/sigma.g[2]^2, scale = sigma.g[2]^2/mu.g[2])+
+        delta_star[3]*dgamma(x, shape = mu.g[3]^2/sigma.g[3]^2, scale = sigma.g[3]^2/mu.g[3])+
+        delta_star[4]*dgamma(x, shape = mu.g[4]^2/sigma.g[4]^2, scale = sigma.g[4]^2/mu.g[4]), add = T, lwd = 2, lty = "dashed", n = 500)
+legend("topright", paste("state", c(1,2,3,4)), col = color, lwd = 2, bty = "n")
+
+
+hist(data7$angle, prob = T, breaks = 50, border = "white", main = NULL, col = "grey70", xlab = "turning angle", ylim = c(0,12))
+curve(delta_star[1]*dbeta(x, alpha[1], beta[1]), add = T, lwd = 2, col = color[1], n = 500)
+curve(delta_star[2]*dbeta(x, alpha[2], beta[2]), add = T, lwd = 2, col = color[2], n = 500)
+curve(delta_star[3]*dbeta(x, alpha[3], beta[3]), add = T, lwd = 2, col = color[3], n = 500)
+curve(delta_star[4]*dbeta(x, alpha[4], beta[4]), add = T, lwd = 2, col = color[4], n = 500)
+curve(delta_star[1]*dbeta(x, alpha[1], beta[1])+
+        delta_star[2]*dbeta(x, alpha[2], beta[2])+
+        delta_star[3]*dbeta(x, alpha[3], beta[3])+
+        delta_star[4]*dbeta(x, alpha[4], beta[4]), add = T, lwd = 2, lty = "dashed", n = 500)
+
+hist(data7$height.fd, prob = T, breaks = 200, xlim = c(-5,5), border = "white", main = NULL, col = "grey70", xlab = "height difference", ylim = c(0,1))
+curve(delta_star[1]*dsn(x, xi[1], omega[1], al[1]), add = T, lwd = 2, col = color[1], n = 500)
+curve(delta_star[2]*dsn(x, xi[2], omega[2], al[2]), add = T, lwd = 2, col = color[2], n = 500)
+curve(delta_star[3]*dsn(x, xi[3], omega[3], al[3]), add = T, lwd = 2, col = color[3], n = 500)
+curve(delta_star[4]*dsn(x, xi[4], omega[4], al[4]), add = T, lwd = 2, col = color[4], n = 500)
+curve(delta_star[1]*dsn(x, xi[1], omega[1], al[1])+
+        delta_star[2]*dsn(x, xi[2], omega[2], al[2])+
+        delta_star[3]*dsn(x, xi[3], omega[3], al[3])+
+        delta_star[4]*dsn(x, xi[4], omega[4], al[4]), add = T, lwd = 2, lty = "dashed", n = 500)
+
+# state dependent distributions unter dem Modell sehen echt kacke aus
+
+par(mfrow = c(3,1))
+plot(data7$step, pch = 20, bty = "n", col = color[states], ylab = "step length")
+plot(data7$angle, pch = 20, bty = "n", col = color[states], ylab = "turning angle")
+plot(data7$height.fd, pch = 20, bty = "n", col = color[states], ylab = "height difference")
+
